03d41919e6916d73fab77324c115776c4a55be74
trrn2011 <trrn2011@gmail.com>
2020-10-23 20:53:38 +0900

    クリップボードにコピ-

--- main.js
+++ main.js
@@ -77,10 +77,24 @@ $(function(){
     $('.formal').on('click', function () {
         input_text = iwake_formal[Math.floor(Math.random() * iwake_formal.length)]
         $('.textarea').val(input_text)
+        let $textarea = $('<textarea></textarea>');
+        $textarea.text(input_text);
+        $(this).append($textarea);
+        $textarea.select();
+        document.execCommand('copy');
+        $textarea.remove();
     });
 
     $('.private').on('click', function () {
         input_text = iwake_private[Math.floor(Math.random() * iwake_private.length)]
         $('.textarea').val(input_text)
+        let $textarea = $('<textarea></textarea>');
+        $textarea.text(input_text);
+        $(this).append($textarea);
+        $textarea.select();
+        document.execCommand('copy');
+        $textarea.remove();
     });
+
+    
 });
